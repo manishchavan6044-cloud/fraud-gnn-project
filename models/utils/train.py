d2c793e2f84ae3e21520d9c266f127e0649f2d17
Manish Chavan <manishchavan6044@gmail.com>
2026-04-28 21:43:56 +0530

    Create train.py

---
+++ models/utils/train.py
@@ -0,0 +1,21 @@
+import torch
+from models.gcn_model import FraudGCN
+from utils.graph_builder import build_graph
+
+data = build_graph("data/transactions.csv")
+
+model = FraudGCN(16, 32, 2)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+loss_fn = torch.nn.CrossEntropyLoss()
+
+for epoch in range(50):
+    model.train()
+    optimizer.zero_grad()
+
+    out = model(data.x, data.edge_index)
+    loss = loss_fn(out, data.y.long())
+
+    loss.backward()
+    optimizer.step()
+
+    print(f"Epoch {epoch}, Loss: {loss.item()}")
